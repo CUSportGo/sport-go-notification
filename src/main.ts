7541e32d929f485fbccd275349a3f7dd5adb8409
ANAN DEESAKUL <62237588+Arnan-Dee@users.noreply.github.com>
2023-10-23 12:26:56 +0700

    Implement RabbitMQ (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const notificationGrpcOption: MicroserviceOptions = {
-    transport: Transport.GRPC,
+    transport: Transport.RMQ,
     options: {
-      package: 'notification',
-      protoPath: join(__dirname, 'proto/notification.proto'),
-      url: 'localhost:8085',
+      urls: ["amqp://localhost:5672"],
+      queue: 'email_queue',
+      queueOptions: { durable: false },
+      prefetchCount: 1,
     },
   };
 
